2bc6c17fbc57f8394fcb84c2c328585eeaac501b
xxn <messagexxq@gmail.com>
2023-05-22 21:55:22 +0800

    feat:deploy

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,52 +1,53 @@
-import * as VueRouter from "vue-router";
-import post from "./view/post.vue";
-import list from "./view/list.vue";
-import detail from "./view/detail.vue";
-import login from "./view/login.vue";
-import register from "./view/register.vue";
+import * as VueRouter from 'vue-router'
+import post from './view/post.vue'
+import list from './view/list.vue'
+import detail from './view/detail.vue'
+import login from './view/login.vue'
+import register from './view/register.vue'
 import center from './view/center.vue'
-import App from "./App.vue";
-import {getToken} from "./utils/token";
-import {useUser} from "./store/user";
-import http from "./http";
+import App from './App.vue'
+import { getToken } from './utils/token'
+import { useUser } from './store/user'
+import http from './http'
 
 export const router = VueRouter.createRouter({
   history: VueRouter.createWebHistory(),
   routes: [
     {
-      path: "/",
+      path: '/',
       component: App,
-      redirect: "/list",
+      children: [],
+      redirect: '/list'
     },
     {
-      path: "/list",
-      component: list,
+      path: '/list',
+      component: list
     },
     {
-      path: "/post",
-      component: post,
+      path: '/post',
+      component: post
     },
     {
-      path: "/detail",
-      component: detail,
+      path: '/detail',
+      component: detail
     },
     {
-      path: "/register",
-      component: register,
+      path: '/register',
+      component: register
     },
     {
-      path: "/login",
-      component: login,
+      path: '/login',
+      component: login
     },
     {
-      path: "/center",
-      component: center,
-    },
-  ],
-});
-router.beforeEach((to,form,next)=>{
+      path: '/center',
+      component: center
+    }
+  ]
+})
+router.beforeEach((to, form, next) => {
   const user = useUser()
-  if(getToken()&&!Object.keys(user.user).length){
+  if (getToken() && !Object.keys(user.user).length) {
     user.fetchUser()
   }
   next()
--- client/src/view/detail.vue
+++ client/src/view/detail.vue
@@ -1,65 +1,54 @@
 <template>
   <div class="detail-wrap">
-    <h1>{{ detail.post?.title||'' }}</h1>
-    <section v-html="detail.post?.Content||''" />
-    <p
-      v-for="(item,i) in detail.post?.comment"
-      :key="item.id"
-    >
-      {{ i+1 }}楼：{{ item.content }}
+    <h1>{{ detail.post?.title }}</h1>
+    <section v-html="detail.post?.Content || ''" />
+    <p v-for="(item, i) in detail.post?.comment" :key="item.id">
+      {{ i + 1 }}楼：{{ item.content }}
     </p>
     <el-form style="margin-top: 25px">
       <el-form-item>
-        <el-input
-          v-model:model-value="comment"
-          type="textarea"
-        />
+        <el-input v-model:model-value="comment" type="textarea" />
       </el-form-item>
       <el-form-item>
-        <el-button
-          type="primary"
-          @click="post"
-        >
-          comment
-        </el-button>
+        <el-button type="primary" @click="post"> comment </el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script lang="ts" setup>
-
-import {onMounted, reactive, ref} from "vue";
-import http from "../http";
-import {useRoute, useRouter} from "vue-router";
+import { onMounted, reactive, ref } from 'vue'
+import http from '../http'
+import { useRoute, useRouter } from 'vue-router'
 const router = useRoute()
 
-let detail = reactive({
-  post:{}
+let detail: any = reactive({
+  post: { title: '', Content: '', comment: [] }
 })
 console.log(router)
 const comment = ref<string>('')
-const post = ()=> {
-  http.post('/api/comment',{postId:router.query?.id,content:comment.value})
-      .then(res=>{
-        if(res.data){
-          comment.value = ''
-          getDetail()
-        }
-      })
-}
-const getDetail = ()=>{
-  http.get(`/api/posts/detail`,{
-    params:{
-      id:router.query?.id
-    }
-  }).then(res=>{
-    if(res.status===200){
-      detail.post = res.data
+const post = () => {
+  http.post('/api/comment', { postId: router.query?.id, content: comment.value }).then(res => {
+    if (res.data) {
+      comment.value = ''
+      getDetail()
     }
   })
 }
-onMounted(()=>{
+const getDetail = () => {
+  http
+    .get(`/api/posts/detail`, {
+      params: {
+        id: router.query?.id
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        detail.post = res.data
+      }
+    })
+}
+onMounted(() => {
   getDetail()
 })
 </script>
@@ -73,7 +62,7 @@ onMounted(()=>{
   }
   p {
     line-height: 35px;
-    :first-child{
+    :first-child {
       margin-top: 25px;
     }
   }
